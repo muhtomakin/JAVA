45a97100ed1cfbc58e6692dae4459f57c01aee0a
muhtomakin <muhtomakin@gmail.com>
2023-01-30 22:52:37 +0100

    JAVA-Learning

---
+++ northwind/src/main/java/kodlamaio/northwind/NorthwindApplication.java
@@ -0,0 +1,13 @@
+package northwind.src.main.java.kodlamaio.northwind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthwindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthwindApplication.class, args);
+	}
+
+}
